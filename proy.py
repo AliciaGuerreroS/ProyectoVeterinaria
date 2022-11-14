d16ad26aa15c5110d016b8733098e197d9b595ab
Alicia <alissipac72@gmail.com>
2022-11-13 22:28:13 -0600

    Agregando algunas modificaciones a la estructura principal

--- proy.py
+++ proy.py
@@ -1,31 +1,56 @@
 
-"""
-Veterinaria necesita revisar y registrar datos de una mascota
+import csv
+from csv import writer
+from datetime import date
+import pandas as pd
 
-Datos de una mascota:
-
-Nombre
-Fecha de nacimiento
-Raza
-Nombre de dueño(a)
-DNI del dueño(a)
-Por el momento no se tomará en cuenta datos relacionados a su histórico de visitas ni peso o talla.
-
-En una veterinaria se requiere una pequeña interfaz por línea de comandos que permita:
-
-Opción 1: Cargar un archivo csv con datos de 5 mascotas. Tras una persona seleccionar esta opción, debe el sistema indicar un mensaje "Se cargaron los datos de 5 mascotas".
-Opción 2: Mostrar datos de mascotas cargadas en el sistema.
-Opción 3: Agregar mascota. En esta opción el sistema solicita los datos de la mascota para su registro.
-Opción 4: Buscar mascota. Al seleccionar esta opción, el sistema indicar subopciones de búsqueda como: nombre mascota, dueño, raza, edad o DNI. Acorde a la opción y valor ingresado se debe mostrar las mascotas que cumplen dichos criterios.
-Opción 5: Ordenar mascota. Al seleccionar esta opción, el sistema indicar subopciones de ordenamiento como: nombre mascota, dueño, raza, edad o DNI. Acorde a la opción y valor ingresado se debe mostrar las mascotas que cumplen dichos criterios.
-Opción 6: Guardar mascotas en archivo de disco duro (.txt o csv)
-Se solicita escribir un programa en Python que permita realizar las gestiones descritas en las opciones líneas arriba. Para ello, se debe utilizar: colecciones (listas, tuplas, etc), funciones y clases de Python. En caso deseen utilizar otros tópicos de Python no hay restricciones.
-
-Nota: mostrar datos de mascota involucra: nombre, edad, raza, dueño y DNI del dueño.
-
-"""
 
 lista_opciones = [1, 2, 3, 4, 5, 6]
+
+Lista_mascotas_cargadas = []
+fila= []
+mascotas = []
+respuesta = ["si", "no"]
+
+
+class Mascota:
+    def __init__(self, nombre, fecha_nacimiento, raza, nombre_dueño, DNI_dueño):
+        self.nombre = nombre
+        self.fecha_nacimiento = fecha_nacimiento
+        self.raza = raza
+        self.nombre_dueño = nombre_dueño
+        self.DNI_dueño = DNI_dueño
+
+
+    def agregar_mascota(self):
+
+        texto1 = f"Nombre: {self.nombre} "
+        texto2 = f"Fecha de nacimiento: {self.fecha_nacimiento}"
+        texto3 = f"Raza: {self.raza}"
+        texto4 = f"Nombre del dueño: {self.nombre_dueño}"
+        Texto5 = f"DNI del dueño: {self.DNI_dueño}"
+        return mascotas.append([texto1, texto2, texto3, texto4, Texto5])
+
+
+class Archivo:
+    def __init__(self, doc) -> None:
+        self.doc= doc
+
+    def cargar_archivo(self):
+        with open("MASCOTAS.csv", encoding= 'utf-8') as f:
+            self.doc = csv.reader(f)
+            #print(self.doc)
+            for linea in self.doc:
+                #print(f"Se cargaron los datos de 5 mascotas")
+                print(f"Se cargaronlos datos de: {len(list(self.doc))} Mascotas")
+
+    def mostrar_datos(self, doc):
+         with open("MASCOTAS.csv", encoding= 'utf-8') as f:
+            self.doc = csv.reader(f)
+            #print(self.doc)
+            df=pd.DataFrame(self.doc)
+            print(df)
+
 
 print("Bienvenidos a la veterinaria")
 print("¿Que accion desea realizar?")
@@ -40,7 +65,7 @@ while True:
     while True:
         try:
 
-            opcion = int(input("Ingresa el numero de la opcion: "))
+            opcion = int(input("Ingresa el numero de la opcion a ejecutar: "))
             #colocar un try en la linea anterior para los errores de typo de datos ingresados y que se vuelva a ingresar
         except: 
             print("Por favor ingresa un numero valido")
@@ -51,94 +76,95 @@ while True:
     if opcion in lista_opciones:
         print(f"Opcion {opcion} ")
 
+
         if opcion == 1:
-            print("Opcion 1")
-            break
+            print("Ejecutando Opcion 1")
+            mi_archivo= Archivo("MASCOTAS.csv")
+            mi_archivo.cargar_archivo()
 
         elif opcion == 2:
-            print("Opcion 2")
-            break
+            print("Ejecutando Opcion 2: MOSTRAR DATOS")
+
 
         elif opcion == 3:
-            print("Opcion 3")
-            break
+            print("Ejecutando Opcion 3 ")
+            
 
         elif opcion == 4:
-            print("Opcion 4")
-            break
+            print("Ejecutando Opcion 4")
+            
 
         elif opcion == 5:
-            print("Opcion 5")
-            break
+            print("Ejecutando Opcion 5")
+            class Archivo:
+                def __init__(self, doc) -> None:
+                    self.doc= doc
+
+                def cargar_archivo(self):
+                    with open("MASCOTAS.csv", encoding= 'utf-8') as f:
+                        self.doc = csv.reader(f)
+                        print(f"Se cargaron los datos de 5 mascotas")
+
+
+                def mostrar_datos(self, doc):
+                    with open("MASCOTAS.csv", encoding= 'utf-8') as f:
+                        self.doc = csv.reader(f)
+                        #print(self.doc)
+                        import pandas as pd
+                        df=pd.DataFrame(self.doc)
+                        print(df)
+
+                        """for linea in self.doc:
+                            print(linea)"""
+
+                def ordenar_datos(self,doc):
+                    with open("MASCOTAS.csv", encoding= 'utf-8') as f:
+                        self.doc = csv.reader(f)
+                        import pandas as pd
+                        df=pd.DataFrame(self.doc)
+                        filtrado=df.loc[1:,[0,1,2,3,4]]
+                        #lista_subopciones=[0,1,2,3]
+                        nombre_subopciones=['nombre de mascota','raza', 'dueño' , 'DNI']
+                        print("OPCION 5: Ordenar datos")
+                        print("Se puede ordenar de acuerdo a los siguientes campos:")
+                        print("1) Ordenar por nombre de mascota")
+                        #print("2) Ordenar por Fecha de Nacimiento")
+                        print("2) Ordenar por Raza")
+                        print("3) Ordenar por Dueño")
+                        print("4) Ordenar por DNI dueño")
+
+                        while True:
+                            opcion = int(input("Ingresa el numero de la subopcion: "))
+
+                            if opcion == 1:
+                                print("Se ordenara de acuerdo",opcion,":",nombre_subopciones[opcion-1])
+                                print(filtrado.sort_values(by=[opcion-1],ascending=[True]))
+                            if opcion in [2,3,4]:
+                                print("Se ordenara de acuerdo",opcion,":",nombre_subopciones[opcion-1])
+                                print(filtrado.sort_values(by=[opcion],ascending=[True]))
+                            else:
+                                print("Ingresar una opcion valida del 1 al 4")
+
+                            respuesta= input("Desea seguir ordenando datos? si/no: ")
+                            if respuesta == "si":
+                                continue
+                            if respuesta == "no":
+                                print("Fue un gusto servirle")
+                                break
+
+            ### estas son las lineas para ejecucion 
+            mi_archivo= Archivo("MASCOTAS.csv")
+            # mi_archivo.cargar_archivo()
+            #mi_archivo.mostrar_datos("MASCOTAS.csv")
+            mi_archivo.ordenar_datos("MASCOTAS.csv")
+            
 
         else:
-            print("Opcion 6")
+            print("Ejecutando Opcion 6")
+
+        respuesta= input("Necesita realizar alguna otra operación? si/no: ")
+        if respuesta == "si":
+         continue
+        if respuesta == "no":
+            print("Estamos para servirle")
             break
-
-    else:
-        print("ingrese el numero de la opcion entre 1 y 6 ")
-
-"""""
-if opcion in lista_opciones:
-    print(f"Opcion {opcion}")   
-
-else:
-    print("ingrese el numero de la opcion entre 1 y 6 ")
-
-Este podria ser el resumen de las lineas 42 en adelante
-"""""
-
-print("Por favor ingrese los datos de la mascota: ")
-class Mascota:
-    def __init__(self, nombre, fecha_nacimiento, raza, nombre_dueño, DNI_dueño):
-        self.nombre = nombre
-        self.fecha_nacimiento = fecha_nacimiento
-        self.raza = raza
-        self.nombre_dueño = nombre_dueño
-        self.DNI_dueño = DNI_dueño
-
-__dict__ = []
-
-nombre = input("Nombre: ")
-fecha_nacimiento = input("Fecha de nacimiento: ")
-raza = input("Raza: ")
-nombre_dueño = input("Nombre del dueño: ")
-DNI_dueño = input("DNI del dueño: ")
-
-#forma1
-numbers= [i for i in range(7,538,10)]
-print(numbers)
-#forma2
-def number_with_seven():
-    for i in range(7,538,10):
-        print(i)
-
-number_with_seven()
-
-###hola estoy intentando arreglar la configuracion en github
-
-
-def common_element2(list1,list2):
-    for i in list1:
-        for j in list2:
-            if i == j:
-               print(i)
-
-def common_element2(list1,list2):
-    for i in list1:
-        for j in list2:
-            if i == j:
-               print(i)
-
-def common_element2(list1,list2):
-    for i in list1:
-        for j in list2:
-            if i == j:
-               print(i)
-
-###Holliisisisisiisisisssssssssssssss
-###Holliisisisisiisisisssssssssssssss
-###Holliisisisisiisisisssssssssssssss
-###Holliisisisisiisisisssssssssssssss
-###Holliisisisisiisisisssssssssssssss
-###Holliisisisisiisisisssssssssssssss
